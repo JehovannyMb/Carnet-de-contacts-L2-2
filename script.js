a8d0a09dbcc9b90efe444accfd3564ae25c3bbfd
Jehovanny Mbenga <mbengajehovanny@gmail.com>
2023-11-22 14:37:47 +0200

    Lire le bouton et ajouter le contact à la liste des contacts

---
+++ script.js
@@ -0,0 +1,37 @@
+let prenom, nom, telephone, groupe, email, bio, dropBox, creer, bouttonCreer, list, icon_btn;
+prenom = document.querySelector("#Prenom");
+nom = document.querySelector("#Nom");
+telephone = document.querySelector("#Telephone");
+groupe = document.querySelector("#Groupe");
+email = document.querySelector("#Email");
+bio = document.querySelector("#Bio");
+dropBox = document.querySelector(".dropBox");
+bouttonCreer = document.querySelector('.creer');
+list = document.querySelector(".list");
+icon_btn = document.querySelector(".list_btn")
+
+console.log(prenom.value);
+bouttonCreer.addEventListener('click', function () {
+    console.log(prenom.value);
+    let tab = document.createElement('div');
+    tab.innerHTML = dropBox.value + "\n" + prenom.value + nom.value + groupe.value + telephone.value + bio.value 
+  
+    list.appendChild(tab);
+    console.log(tab);
+
+});
+addContact()
+
+// function showContacts() {
+
+// }
+
+
+
+// function saveData(newContact) {
+//     localStorage.setItem("contact", this.newContact);
+
+// }
+// function getData(newContact) {
+//     const getData = createElement('div');
+// }
