5252ada9729744a2054c54f66b20f8d8be72136a
Jehovanny Mbenga <mbengajehovanny@gmail.com>
2023-11-27 01:49:06 +0200

    All validations

--- script.js
+++ script.js
@@ -15,12 +15,13 @@ console.log(nom.value);
 
 
 bouttonCreer.addEventListener('click', function () {
-    if (telephone.value == "" || nom.value == "" || prenom.value == "") {
-
-        validationTel()
-        validationNom()
+    if (telephone.value == "" || nom.value == "" || prenom.value == "" || groupe.value == "") {
         validationPrenom()
+        validationNom()
+        validationTel()
         validationGroupe()
+        validationEmail()
+        validationBio()
     }
     else {
         console.log(prenom.value);
@@ -49,193 +50,253 @@ bouttonCreer.addEventListener('click', function () {
 
 function validationPrenom() {
     const form = document.querySelector('form')
-    let p = document.querySelector('.error')
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         validatePrenom(form.Prenom)
 
     })
 
-
     function validatePrenom(item) {
         let prenomError = document.querySelector('#prenomError')
-        let regex = /[A-Za-z]{3,15}/
+        let regex = /[A-Za-z]{3,50}/
 
-        if (item.value.length >= 3 && item.value.length <= 15) {
-            item.removeAttribute('style')
+        if (item.value == "") {
+            item.setAttribute('style', 'border: solid red;')
+            prenomError.innerHTML = "<span style= 'color : red'>Veillez renseigner votre prenom</span>"
 
-            if (regex.test(item.value)) {
-                console.log('letters is coorectly writen');
-
-
-
-                prenomError.innerHTML = ""
+        } else {
+            if (item.value.length >= 3 && item.value.length <= 50) {
                 item.removeAttribute('style')
 
+                if (regex.test(item.value)) {
+                    prenomError.innerHTML = ""
+                    item.removeAttribute('style')
+
+                } else {
+                    item.setAttribute('style', 'border: solid red;')
+                    prenomError.innerHTML = "<span style= 'color : red'> Le caractères doivent être des lettres </span>"
+                    item.value.innerHTML = ""
+
+                }
             } else {
                 item.setAttribute('style', 'border: solid red;')
-                prenomError.innerHTML = "<span style= 'color : red'> Le caractères doivent être des lettres </span>"
-                item.value.innerHTML = ""
+                prenomError.innerHTML = "<span style= 'color : red'> Le champ doit contenir entre 3 et 50 caractères</span>"
 
             }
-        } else {
-            item.setAttribute('style', 'border: solid red;')
-            prenomError.innerHTML = "<span style= 'color : red'> Le champ doit contenir entre 3 et 15 caractères</span>"
 
         }
-
-
 
 
 
     }
 }
 
-
-
-
-
-
 function validationNom() {
     const form = document.querySelector('form')
-    let p = document.querySelector('.error')
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         validateName(form.Nom)
 
     })
 
-
     function validateName(item) {
         let nomError = document.querySelector('#nomError')
-        let regex = /[A-Za-z]{3,15}/
+        let regex = /[A-Za-z]{3,50}/
 
-        if (item.value.length >= 3 && item.value.length <= 15) {
-            item.removeAttribute('style')
+        if (item.value == "") {
+            item.setAttribute('style', 'border: solid red;')
+            nomError.innerHTML = "<span style= 'color : red'>Veillez renseigner votre nom</span>"
 
-            if (regex.test(item.value)) {
-                console.log('letters is coorectly writen');
-
-
-
-                nomError.innerHTML = ""
+        } else {
+            if (item.value.length >= 3 && item.value.length <= 50) {
                 item.removeAttribute('style')
 
+                if (regex.test(item.value)) {
+                    nomError.innerHTML = ""
+                    item.removeAttribute('style')
+
+                } else {
+                    item.setAttribute('style', 'border: solid red;')
+                    nomError.innerHTML = "<span style= 'color : red'> Le caractères doivent être des lettres </span>"
+                    item.value.innerHTML = ""
+
+                }
             } else {
                 item.setAttribute('style', 'border: solid red;')
-                nomError.innerHTML = "<span style= 'color : red'> Le caractères doivent être des lettres </span>"
-                item.value.innerHTML = ""
+                nomError.innerHTML = "<span style= 'color : red'> Le champ doit contenir entre 3 et 50 caractères</span>"
 
             }
-        } else {
-            item.setAttribute('style', 'border: solid red;')
-            nomError.innerHTML = "<span style= 'color : red'> Le champ doit contenir entre 3 et 15 caractères</span>"
 
         }
-
-
-
-
 
     }
 }
 
-
-
-
-
 function validationTel() {
     const form = document.querySelector('form')
-    let p = document.querySelector('.error')
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         validateTelephone(form.Telephone)
 
     })
 
-
     function validateTelephone(item) {
         let telError = document.querySelector('#telError')
         let regex = /085|084|082|081|099|097/
 
-        if (item.value.length == 10) {
-            item.removeAttribute('style')
+        if (item.value == "") {
+            item.setAttribute('style', 'border: solid red;')
+            telError.innerHTML = "<span style= 'color : red'>Veillez renseigner votre numéro de téléphone</span>"
 
-            if (regex.test(item.value)) {
-                console.log('letters is coorectly writen');
+        } else {
+            if (item.value.length == 10) {
+                item.removeAttribute('style')
 
+                if (regex.test(item.value)) {
+                    telError.innerHTML = ""
+                    item.removeAttribute('style')
 
+                } else {
+                    item.setAttribute('style', 'border: solid red;')
+                    telError.innerHTML = "<span style= 'color : red'> Le numero doit être un numéro national. Ex : 082.... ou 099...</span>"
+                    item.value.innerHTML = ""
 
-                telError.innerHTML = ""
-            item.removeAttribute('style')
-
+                }
             } else {
                 item.setAttribute('style', 'border: solid red;')
-                telError.innerHTML = "<span style= 'color : red'> Le numero doit être un numéro national. Ex : 082.... ou 099...</span>"
-                item.value.innerHTML = ""
+                telError.innerHTML = "<span style= 'color : red'> Le champ doit contenir 10 chiffres</span>"
 
             }
-        } else {
-            item.setAttribute('style', 'border: solid red;')
-            telError.innerHTML = "<span style= 'color : red'> Le champ doit contenir 10 chiffres</span>"
 
         }
-
-
 
 
 
     }
 }
 
-
-
 function validationGroupe() {
     const form = document.querySelector('form')
-    let p = document.querySelector('.error')
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         validateGroupe(form.Groupe)
 
     })
 
-
     function validateGroupe(item) {
         let groupeError = document.querySelector('#groupeError')
         let regex = /[A-Za-z]{3,15}/
 
-        if (item.value.length >= 3 && item.value.length <= 15) {
-            item.removeAttribute('style')
+        if (item.value == "") {
+            item.setAttribute('style', 'border: solid red;')
+            groupeError.innerHTML = "<span style= 'color : red'>Veillez renseigner votre groupe</span>"
 
-            if (regex.test(item.value)) {
-                console.log('letters is coorectly writen');
+        } else {
 
-
-
-                groupeError.innerHTML = ""
+            if (item.value.length >= 3 && item.value.length <= 50) {
                 item.removeAttribute('style')
 
+                if (regex.test(item.value)) {
+                    groupeError.innerHTML = ""
+                    item.removeAttribute('style')
+
+                } else {
+                    item.setAttribute('style', 'border: solid red;')
+                    groupeError.innerHTML = "<span style= 'color : red'> Le caractères doivent être des lettres </span>"
+                    item.value.innerHTML = ""
+
+                }
             } else {
                 item.setAttribute('style', 'border: solid red;')
-                groupeError.innerHTML = "<span style= 'color : red'> Le caractères doivent être des lettres </span>"
-                item.value.innerHTML = ""
+                groupeError.innerHTML = "<span style= 'color : red'> Le champ doit contenir entre 3 et 50 caractères</span>"
 
             }
-        } else {
-            item.setAttribute('style', 'border: solid red;')
-            groupeError.innerHTML = "<span style= 'color : red'> Le champ doit contenir entre 3 et 15 caractères</span>"
-
         }
-
-
-
-
 
     }
 }
 
+function validationEmail() {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        validateEmail(form.Email)
 
+    })
 
+    function validateEmail(item) {
+        let emailError = document.querySelector('#emailError')
+        let regex = /[A-Za-z0-9-_]+\.@[A-Za-z]{2,15}\.[a-z]{2,10}/
+
+        if (item.value == "") {
+            item.setAttribute('style', 'border: solid red;')
+            emailError.innerHTML = "<span style= 'color : red'>Veillez renseigner votre mail</span>"
+
+        } else {
+
+            if (item.value.length >= 3 && item.value.length <= 50) {
+                item.removeAttribute('style')
+
+                if (regex.test(item.value)) {
+                    emailError.innerHTML = ""
+                    item.removeAttribute('style')
+
+                } else {
+                    item.setAttribute('style', 'border: solid red;')
+                    emailError.innerHTML = "<span style= 'color : red'> Le caractères doivent être des lettres </span>"
+                    item.value.innerHTML = ""
+
+                }
+            } else {
+                item.setAttribute('style', 'border: solid red;')
+                emailError.innerHTML = "<span style= 'color : red'> Le champ doit contenir entre 3 et 50 caractères</span>"
+
+            }
+        }
+
+    }
+}
+
+function validationBio() {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        validateBio(form.Bio)
+
+    })
+
+    function validateBio(item) {
+        let bioError = document.querySelector('#bioError')
+        let regex = /[.]+/
+
+        if (item.value == "") {
+            item.setAttribute('style', 'border: solid red;')
+            bioError.innerHTML = "<span style= 'color : red'>Veillez renseigner votre biographie</span>"
+
+        } else {
+
+            if (item.value.length >= 3 && item.value.length <= 250) {
+                item.removeAttribute('style')
+
+                if (regex.test(item.value)) {
+                    bioError.innerHTML = ""
+                    item.removeAttribute('style')
+
+                } else {
+                    item.setAttribute('style', 'border: solid red;')
+                    bioError.innerHTML = "<span style= 'color : red'> Le caractères doivent être des lettres </span>"
+                    item.value.innerHTML = ""
+
+                }
+            } else {
+                item.setAttribute('style', 'border: solid red;')
+                bioError.innerHTML = "<span style= 'color : red'> Le champ doit contenir entre 3 et 250 caractères</span>"
+
+            }
+        }
+
+    }
+}
 
 
 
