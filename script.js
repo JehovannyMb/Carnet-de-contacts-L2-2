26a0bcd24832caa0810cb10645f6507bbd8affe5
victoireilunga <ilungavictoire791@gmail.com>
2023-12-04 11:26:25 +0200

    affichage

--- script.js
+++ script.js
@@ -1,45 +1,73 @@
 
-let prenom, nom, telephone, groupe, email, bio, dropBox, creer, bouttonCreer, list, icon_btn;
-prenom = document.querySelector("#Prenom");
-nom = document.querySelector("#Nom");
-telephone = document.querySelector("#Telephone");
-groupe = document.querySelector("#Groupe");
-email = document.querySelector("#Email");
-bio = document.querySelector("#Bio");
-dropBox = document.querySelector(".dropBox");
-bouttonCreer = document.querySelector('.creer');
-list = document.querySelector(".list");
-icon_btn = document.querySelector(".list_btn")
+// let prenom, nom, telephone, groupe, email, bio, dropBox, creer, bouttonCreer, list, icon_btn;
+// prenom = document.querySelector("#Prenom");
+// nom = document.querySelector("#Nom");
+// telephone = document.querySelector("#Telephone");
+// groupe = document.querySelector("#Groupe");
+// email = document.querySelector("#Email");
+// bio = document.querySelector("#Bio");
+// dropBox = document.querySelector(".dropBox");
+// bouttonCreer = document.querySelector('.creer');
+// list = document.querySelector(".list");
+// icon_btn = document.querySelector(".list_btn")
 
-console.log(prenom.value);
-bouttonCreer.addEventListener('click', function () {
-  console.log(prenom.value);
-  let tab = document.createElement('div');
-  tab.innerHTML = dropBox.value + "\n" + prenom.value + nom.value + groupe.value + telephone.value + bio.value
+// console.log(prenom.value);
+// bouttonCreer.addEventListener('click', function () {
+//   console.log(prenom.value);
+//   let tab = document.createElement('div');
+//   tab.innerHTML = dropBox.value + "\n" + prenom.value + nom.value + groupe.value + telephone.value + bio.value
 
-  list.appendChild(tab);
-  console.log(tab);
-  let imgElement = document.createElement('div')
-  // imgElement.setAttribute('style', 'width: 25%; height: 25vh; border-radius: 50%; border: 1px solid black');
+//   list.appendChild(tab);
+//   console.log(tab);
+//   let imgElement = document.createElement('div')
+//   // imgElement.setAttribute('style', 'width: 25%; height: 25vh; border-radius: 50%; border: 1px solid black');
 
-  tab.setAttribute('style', 'width: 25%; height: 25vh; border-radius: 50%; border: 1px solid black; text-align: center; padding-top: 5em;');
+//   tab.setAttribute('style', 'width: 25%; height: 25vh; border-radius: 50%; border: 1px solid black; text-align: center; padding-top: 5em;');
 
-  tab.innerHTML = imgElement
-  // list.appendChild(imgElement)
-  imgElement.appendChild(dropBox.value)
-  let
+//   tab.innerHTML = imgElement
+//   // list.appendChild(imgElement)
+//   imgElement.appendChild(dropBox.value)
+//   let
 
-});
-// addContact()
+// });
+// // addContact()
 
 
 
-// Fonction d'affichage d'image
-const dropBoximg = document.querySelector('.dropBox');
+// // Fonction d'affichage d'image
+// const dropBox = document.querySelector('.dropBox');
+// const uploadFile = document.getElementById('uploadFile');
+
+// // Écouter l'événement de déposer
+// uploadFile.addEventListener('change', (e) => ('change', (e) => {
+//   e.preventDefault();
+//   console.log('dropped');
+//   const file = e.target.files[0];
+//   const image = document.createElement('img');
+
+//   const reader = new FileReader();
+
+//   reader.onload = (e) => {
+//     image.src = e.target.result;
+//     dropBox.appendChild(image);
+//   };
+//   reader.readAsDataURL(file);
+
+// });
+
+// // Empêcher le comportement par défaut du navigateur pour les événements de glisser-déposer
+// dropBox.addEventListener('dragover', (e) => {
+//   e.preventDefault();
+// });
+
+
+
+
+const dropBox = document.querySelector('.dropBox');
 const uploadFile = document.getElementById('uploadFile');
 
 // Écouter l'événement de déposer
-dropBox.addEventListener('change', (e) => {
+uploadFile.addEventListener('change', (e) => {
   e.preventDefault();
   console.log('dropped');
   const file = e.target.files[0];
@@ -49,7 +77,7 @@ dropBox.addEventListener('change', (e) => {
 
   reader.onload = (e) => {
     image.src = e.target.result;
-    dropBoximg.appendChild(image);
+    dropBox.appendChild(image);
   };
   reader.readAsDataURL(file);
 
@@ -60,17 +88,31 @@ dropBox.addEventListener('dragover', (e) => {
   e.preventDefault();
 });
 
+// // Fonction pour réafficher les informations sur le formulaire
+// function afficherInformations() {
+//   // Récupérer les éléments du formulaire
+//   const inputNom = document.getElementById("nom");
+//   const inputAge = document.getElementById("age");
+//   const inputEmail = document.getElementById("email");
 
-// Fonction pour réafficher les informations sur le formulaire
-function afficherInformations() {
-  // Récupérer les éléments du formulaire
-  const inputNom = document.getElementById("nom");
-  const inputAge = document.getElementById("age");
-  const inputEmail = document.getElementById("email");
+//   // Afficher les informations sur le formulaire
 
-  // Afficher les informations sur le formulaire
+// };
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
